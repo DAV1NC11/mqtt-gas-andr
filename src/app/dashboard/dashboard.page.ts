01fc68a6dffd5c30067b77d687acc462940215fa
DAV1NC11 <chagajohn@outlook.com>
2023-05-23 10:28:25 +0300

    dashboards added

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
+
 export class DashboardPage implements OnInit {
+  percentage: number = 75; // Set your desired percentage value
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
+
+
